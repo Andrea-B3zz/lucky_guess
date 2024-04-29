48662a419837998f9130d4211a1ef8134b3e53bf
Andrea Bezzolato <bezz0001@hz.nl>
2024-04-29 11:19:33 +0200

    Basic work implemented, manually managing the database

--- game/views.py
+++ game/views.py
@@ -1,17 +1,43 @@
-from django.db.models import F
-from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.views import generic
+import math
+from random import random
+from django.shortcuts import render
 
 from .models import Guesses, CorrectNumber
 
 # Create your views here.
 def mainView(request):
-    toDisplay = CorrectNumber.objects.order_by("-id")[:1]
-    toDisplay=toDisplay[0]
-    return render(
-            request,
-            "game/mainView.html",
-            {'correctNumber': toDisplay}
-        )
+     toDisplay = CorrectNumber.objects.order_by("-id")[:1]
+     toDisplay=toDisplay[0]
+     test=True
+
+     #if the request is a post, then we're trying to generate a new number, otherwise it's just the homepage
+     if request.method == "POST":
+          while test:
+               numberGenerator = random()
+               value = math.floor(1 + (numberGenerator * (toDisplay.maxNumber - 1)))
+               allGuesses=Guesses.objects.filter(correctNumber_id=toDisplay.pk)
+               
+               for x in allGuesses:
+                    if x.number==value :
+                         test=True
+                         break
+                    test=False
+                    pass
+                    
+          guess = Guesses()
+          guess.number=value
+          guess.correctNumber_id=toDisplay.pk
+          guess.save()
+          return render(
+                request,
+                "game/mainView.html",
+                {'correctNumber': toDisplay,
+                 'numberExtracted': guess.number}
+            )
+     else:
+          return render(
+                request,
+                "game/mainView.html",
+                {'correctNumber': toDisplay,
+                 'numberExtracted': ""}
+            )
